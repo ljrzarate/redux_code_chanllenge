1cdf9fe9348d2a9174557599f896e5db57138d2d
Luis Rodriguez <luis.rodriguez@gorillalogic.com>
2021-04-17 13:19:33 -0500

    setting up credential to su GMAIL as SMTP server

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,19 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Mail
+  config.action_mailer.delivery_method = :smtp
+  host = 'redux-code-challenge.herokuapp.com/'
+  config.action_mailer.default_url_options = { host: host }
+
+  # SMTP settings for gmail
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :user_name            => ENV['EMAIL_USERNAME'],
+    :password             => ENV['EMAIL_PASSWORD'],
+    :authentication       => "plain",
+    :enable_starttls_auto => true
+  }
 end
