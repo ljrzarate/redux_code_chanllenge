97b19c2982ddb55caf0de5e21f25d700e61ccc35
Luis Rodriguez <luis.rodriguez@gorillalogic.com>
2021-04-18 12:43:49 -0500

    add useranmes tables

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
+    @users = User.all.map{ |u| [u.username, u.email] }
     @post = Post.new
     @posts = Post.includes(:comments, :likes).order("created_at desc")
   end
